68740d7ae71c60d79ada6f462c49dc16e2f5c754
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-07-26 15:55:53 +0300

    completed assignment

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import React from "react"
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+ 
+  const [formData, setFormData] = useState({ text: "", category: "" });
+  const optionsList = categories.map((category) => (
+    <option key={category} value={category}>
+      {category}
+    </option>
+  ));
+
+  const handleOnChange = (e) => {
+    const newObj = { ...formData, [e.target.name]: e.target.value };
+    setFormData(newObj);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    onTaskFormSubmit(formData);
+  };
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleFormSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text"  value={formData.text}
+          onChange={handleOnChange}/>
       </label>
       <label>
         Category
-        <select name="category">
+        <select name="category"    value={formData.category}
+          onChange={handleOnChange}>
           {/* render <option> elements for each category here */}
+          {optionsList}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
+function TaskList({ tasks, onTaskDelete }) {
+  const taskList = tasks.map((task, index) => (
+    <Task
+      key={index}
+      text={task.text}
+      category={task.category}
+      onTaskDelete={() => onTaskDelete(index)}
+    />
+  ));
+  return(
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {taskList}
     </div>
   );
 }
